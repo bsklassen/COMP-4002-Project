d4d0d3827acf02d24cc28b0a88e9a33a81cc4cf9
ladamo-rrc <ladamo@rrc.ca>
2026-03-26 21:19:28 -0500

    slowly moving everything over.

---
+++ apps/backend/src/api/v1/controllers/itemController.tsx
@@ -0,0 +1,14 @@
+import { getItems, removeItemById } from "../data/itemData"
+
+
+// Business logic
+
+import type { Item } from "../types/items"
+import * as itemRepository from "../repositories/itemRepository"
+
+export function droppedItemRandomizer(): Item[]{
+    const items = itemRepository.getItems()
+    const droppedItemAmount = Math.random() <0.5 ? 2 : 3;  // will decide to drop either 2 or 3 items
+    const randomItems = [...items].sort(() => Math.random() - 0.5);
+    return randomItems.slice(0, droppedItemAmount);
+}
---
+++ apps/backend/src/api/v1/data/itemData.ts
@@ -0,0 +1,23 @@
+
+import type { Item } from "../types/items"
+
+let items: Item[]= [
+    {id: 1, name: "Minor Health Potion"},
+    {id: 2, name: "Broken Straight Sword"},
+    {id: 3, name: "A rusted nail"},
+    {id: 4, name: "An all-seeing eyeball"},
+    {id: 5, name: "A jar of the ghosts of your past"},
+    {id: 6, name: "The claw of a three-winged heron"},
+    {id: 7, name: "The heart of a siren"},
+    {id: 8, name: "A B.F sword"},
+    {id: 9, name: "A feather from a fallen angel"},
+    {id: 10, name: "The key to the universe"},
+]
+
+export function getItems(): Item[]{
+    return items;
+}
+
+export function removeItem(id: number): void {
+    items = items.filter(item => item.id != id);
+}
