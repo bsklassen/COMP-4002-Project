b4b86cebbef292c40a593150d977c2a25f32a4d2
ladamo-rrc <ladamo@rrc.ca>
2026-03-29 15:16:58 -0500

    Added item repository and made sure everything works with prisma.

--- apps/backend/src/api/v1/services/itemService.ts
+++ apps/backend/src/api/v1/services/itemService.ts
@@ -1,13 +1,13 @@
 import type { Item } from "../types/items"
-import * as itemRepository from "../data/itemData"
+import { itemRepository } from "../repositories/itemRepository";
 
 export const getDroppedItems = async (): Promise<Item[]> => {
     try {
-        const items = itemRepository.getItems();
+        const items = await itemRepository.getAllItems();
         const dropCount = Math.random() < 0.5 ? 2 : 3;
         const droppedItems = [...items]
-        .sort(() => Math.random() - 0.5)
-        .slice(0, dropCount);
+            .sort(() => Math.random() - 0.5)
+            .slice(0, dropCount);
         return droppedItems;
     } catch(error) {
         throw new Error("failed to get items")
@@ -15,10 +15,11 @@ export const getDroppedItems = async (): Promise<Item[]> => {
 }
 
 export const discardItems = async (itemIds: number[]): Promise<void> => {
-
     try {
-        itemIds.forEach((id) => itemRepository.removeItemById(id));
-    } catch(error){
+        for (const id of itemIds) {
+            await itemRepository.delete(id);
+        }
+    } catch(error) {
         throw new Error("failed to discard items")
     }
 }
